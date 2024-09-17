270f0211c4a2c14e11881b41e1ad9f990f2fb245
Raghvendra Pratap Singh <raghvendraps9794@gmail.com>
2024-09-17 23:54:12 +0530

    datatypes

--- day1_variable.js
+++ day1_variable.js
@@ -2,8 +2,18 @@
 how to print console in tabular form
 */
 
-let num1="1234";
+let num1=1234;
 var name2="bjsdds";
 const password="1234";
 
 console.table([num1, name2, password]);
+
+
+// Try to avoid use of var because of functional scope and block scope
+
+
+
+// We can know any variables type buy using below->
+
+console.log(typeof name2);
+console.log(typeof 1234);
